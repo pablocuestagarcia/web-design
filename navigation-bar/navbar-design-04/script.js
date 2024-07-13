eac91ef0a855d6701ab4a7e3d845583ad130f852
pd542145_gsk <pablo.x.delacuestagarcia@gsk.com>
2024-07-13 16:00:18 +0200

    navbars designs

---
+++ navigation-bar/navbar-design-04/script.js
@@ -0,0 +1,7 @@
+document.getElementById('menu-icon').addEventListener('click', function() {
+    document.getElementById('menu').style.left = '0';
+});
+
+document.getElementById('close-icon').addEventListener('click', function() {
+    document.getElementById('menu').style.left = '-300px';
+});
